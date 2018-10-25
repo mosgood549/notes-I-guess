7c860c6845a98c7521dc4309a8a70636b64b9e92
mosgood549 <marley549@icloud.com>
2018-10-25 14:13:41 -0500

    worked on some mirror

--- filters_demo.py
+++ filters_demo.py
@@ -21,34 +21,46 @@ from PIL import Image
 #        pixmap[i,j] = (r,g,b)
 #        
 
-def violet_haze(image):
-    img = Image.open(image)
-    img.show() 
+#def violet_haze(image):
+#    img = Image.open(image)
+#    img.show() 
+#    pixmap = img.load()
+#    
+#    for i in range(img.size[0]):
+#        for j in range(img.size[1]):
+#            r, g, b = pixmap[i,j]
+#            r += 100
+#            g -= 50
+#            b += 500
+#        
+#            pixmap[i,j] = (r,g,b)
+#    for i in range(img.size[0]):
+#        for j in range(0,img.size[1],20):
+#            r, g, b = pixmap[i,j]
+#            r += 10*5
+#            g += 10*2
+#            b += 10
+#        
+#            pixmap[i,j] = (b,g,b)
+#    img.show()
+#    img.save("violet_filter.jpg")
+#    
+def mirror(img):
     pixmap = img.load()
-    
     for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            r, g, b = pixmap[i,j]
-            r += 100
-            g -= 50
-            b += 500
-        
-            pixmap[i,j] = (r,g,b)
-    for i in range(img.size[0]):
-        for j in range(0,img.size[1],20):
-            r, g, b = pixmap[i,j]
-            r += 10*5
-            g += 10*2
-            b += 10
-        
-            pixmap[i,j] = (b,g,b)
-    img.show()
-    img.save("violet_filter.jpg")
+        for j in range(img.size[1]//2):
+            pixmap[i,j] = pixmap[i%(img.size[0]//2),j]
+
+             
+    img.show()        
+
     
 
         
 def main():
-    violet_haze("beach.jpg")
+    img = Image.open("beach.jpg")
+    img.show()
+    mirror(img)
     
     
 if __name__ == "__main__":
